04191ff3fae492d6cbd7f0e0b5e8659cef19316d
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-06-10 20:12:54 +0000

    tagging desiDataModel/0.0.2

--- py/desiDataModel/__init__.py
+++ py/desiDataModel/__init__.py
@@ -1,7 +1,5 @@
 # License information goes here
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-# The line above will help with 2to3 support.
 """
 =============
 desiDataModel
@@ -11,40 +9,14 @@ This package provides support for the DESI_ Data Model.
 
 .. _DESI: http://desi.lbl.gov
 """
-
-def version():
-    """Returns the version of this package.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    version : str
-        A PEP 386-compatible version string.
-
-    Notes
-    -----
-    The version string should be compatible with `PEP 386`_ and
-    `PEP 440`_.
-
-    .. _`PEP 386`: http://legacy.python.org/dev/peps/pep-0386/
-    .. _`PEP 440`: http://legacy.python.org/dev/peps/pep-0440/
-    """
-    from desiUtil.install import get_svn_devstr, most_recent_tag
-    headurl = "$HeadURL$"
-    if headurl.find('tags') > 0:
-        myversion = headurl[headurl.find('tags')+5:].split('/')[0]
-    elif (headurl.find('trunk') > 0) or (headurl.find('branches') > 0):
-        url = headurl[10:len(headurl)-2]
-        findstr = ('branches','trunk')[int(headurl.find('trunk') > 0)]
-        tagurl = url[0:url.find(findstr)]+'tags'
-        myversion = most_recent_tag(tagurl) + '.dev' + get_svn_devstr()
-    else:
-        myversion = '0.0.1.dev'
-    return myversion
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
+from desiUtil.install import version
 #
+# Set version string.
 #
+__version__ = version('$HeadURL$')
 #
-__version__ = version()
+# Clean up namespace
+#
+del version
