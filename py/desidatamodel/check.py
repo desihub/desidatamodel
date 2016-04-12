3b7adf796bf5654f0aac793a18dbf75505515e04
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-04-12 12:46:55 -0400

    add function to scan for rst files

--- py/desidatamodel/check.py
+++ py/desidatamodel/check.py
@@ -12,6 +12,28 @@ from __future__ import (absolute_import, division, print_function,
 # The line above will help with 2to3 support.
 
 
+def scan_model(root):
+    """Find all data model files in a top-level directory.
+
+    Parameters
+    ----------
+    root : :class:`str`
+        Directory to scan.
+
+    Returns
+    -------
+    :class:`list`
+        The data model files found
+    """
+    from os import walk
+    from os.path import join
+    scan = list()
+    for dirpath, dirnames, filenames in walk(root):
+        scan += [join(dirpath, f) for f in filenames
+                 if f.endswith('.rst') and f != 'index.rst']
+    return scan
+
+
 def main():
     """Entry point for the check_model script.
 
@@ -20,4 +42,15 @@ def main():
     :class:`int`
         An integer suitable for passing to :func:`sys.exit`.
     """
+    from os.path import basename
+    from sys import argv
+    desc = """Check actual files against the data model for validity.
+"""
+    parser = ArgumentParser(description=desc, prog=basename(argv[0]))
+    parser.add_argument('-r', '--root', metavar='DIR',
+                        help='Set the section of the data model.')
+    parser.add_argument('directory', metavar='DIR',
+                        help='Check files in this top-level directory.')
+    options = parser.parse_args()
+    scan_model(options.root)
     return 0
--- py/desidatamodel/test/test_check.py
+++ py/desidatamodel/test/test_check.py
@@ -6,16 +6,38 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 #
-import os
+from os import environ
+from os.path import dirname, isdir, join
 import unittest
+from ..check import scan_model
 
 
 class TestCheck(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data_dir = os.path.join(os.path.dirname(__file__), 't')
+        cls.data_dir = join(dirname(__file__), 't')
+        if 'DESIDATAMODEL' in environ:
+            cls.old_env = environ['DESIDATAMODEL']
+        else:
+            cls.old_env = None
+        environ['DESIDATAMODEL'] = dirname(  # root/
+                                           dirname(  # py/
+                                           dirname(  # desidatamodel/
+                                           dirname(__file__)  # test/
+                                           )))
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        if cls.old_env is None:
+            del environ['DESIDATAMODEL']
+        else:
+            environ['DESIDATAMODEL'] = cls.old_env
+
+    def test_scan_model(self):
+        """Test identification of data model files.
+        """
+        root = join(environ['DESIDATAMODEL'], 'doc', 'examples')
+        files = scan_model(root)
+        expected = set([join(root, f) for f in ('sdR.rst', 'spPlate.rst')])
+        self.assertEqual(set(files), expected)
