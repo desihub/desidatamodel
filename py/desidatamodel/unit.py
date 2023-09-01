140a6ada39144683fcb74c04a2c7dc64ca726685
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2023-09-01 14:00:43 -0700

    try to import from desiutil

--- py/desidatamodel/unit.py
+++ py/desidatamodel/unit.py
@@ -11,47 +11,58 @@ from astropy.units import Unit
 from desiutil.log import log
 from . import DataModelError
 
+def _validate_unit(unit, error=False):
+    """Check units for consistency with FITS standard, while allowing
+    some special exceptions.
+
+    Parameters
+    ----------
+    unit : :class:`str`
+        The unit to parse.
+    error : :class:`bool`, optional
+        If ``True``, failure to interpret the unit raises an
+        exception.
+
+    Returns
+    -------
+    :class:`str`
+        If a special exception is detected, the name of the unit
+        is returned.  Otherwise, ``None``.
+
+    Raises
+    ------
+    :exc:`ValueError`
+        If `error` is set and the unit can't be parsed.
+    """
+    acceptable_units = ('maggie', 'maggy', 'mgy',
+                        'electron/Angstrom',
+                        'log(Angstrom)')
+    try:
+        au = Unit(unit, format='fits')
+    except ValueError as e:
+        bad_unit = str(e).split()[0]
+        if any([u in bad_unit for u in acceptable_units]):
+            return bad_unit
+        else:
+            if error:
+                log.critical(str(e))
+                raise
+            else:
+                log.warning(str(e))
+    return None
+
+
+try:
+    from desiutil.annotate import validate_unit
+except ImportError:
+    validate_unit = _validate_unit
+
 
 class DataModelUnit(object):
     """Allow unit-handling code to be shared with several classes.
     """
-    _acceptable_units = ('maggie', 'maggy', 'mgy',
-                         'electron/Angstrom',
-                         'log(Angstrom)')
 
     def check_unit(self, unit, error=False):
-        """Check units for consistency with FITS standard, while allowing
-        some special exceptions.
+        return validate_unit(unit, error=error)
 
-        Parameters
-        ----------
-        unit : :class:`str`
-            The unit to parse.
-        error : :class:`bool`, optional
-            If ``True``, failure to interpret the unit raises an
-            exception.
-
-        Returns
-        -------
-        :class:`str`
-            If a special exception is detected, the name of the unit
-            is returned.  Otherwise, ``None``.
-
-        Raises
-        ------
-        :exc:`ValueError`
-            If `error` is set and the unit can't be parsed.
-        """
-        try:
-            au = Unit(unit, format='fits')
-        except ValueError as e:
-            bad_unit = str(e).split()[0]
-            if any([u in bad_unit for u in self._acceptable_units]):
-                return bad_unit
-            else:
-                if error:
-                    log.critical(str(e))
-                    raise
-                else:
-                    log.warning(str(e))
-        return None
+    check_unit.__doc__ = validate_unit.__doc__
